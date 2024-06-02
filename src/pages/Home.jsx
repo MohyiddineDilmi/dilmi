ad27001ab42b45a1347c303ea7b8153583b47934
Dilmi <33746487+MohyiddineDilmi@users.noreply.github.com>
2024-06-02 18:43:31 -0400

    design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,15 +6,15 @@ import Academic from '../sections/Academic';
 import Professional from '../sections/Professional';
 import Header from '../sections/Header'
 import Footer from '../sections/Footer'
-import { useScroll, useTransform, motion, useMotionTemplate, useMotionValue, animate } from "framer-motion"; // Import framer-motion functions and hooks
-import { useRef, useEffect } from "react"; // Import useRef and useEffect hooks from React
+import { useScroll, useTransform, motion, useMotionTemplate, useMotionValue, animate } from "framer-motion"; 
+import { useRef, useEffect } from "react"; 
 
 
 function Home() {
 
-  const COLORS = ['#18FF04', '#AD00FF', '#00FFD1', '#00D1FF', '#FF6F07','#121212']; // Define an array of colors
-  const color = useMotionValue(COLORS[0]); // Initialize motion value with the first color
-  const backgroundImage = useMotionTemplate`radial-gradient(200% 150% at 0% 10%, #121212 50%, ${color})`; // Create a radial gradient template for background
+  const COLORS = ['#18FF04','#18FF04', '#AD00FF', '#00FFD1', '#00D1FF', '#FF6F07']; 
+  const color = useMotionValue(COLORS[0]);  
+  const backgroundImage = useMotionTemplate`radial-gradient(200% 150% at 0% 10%, #121212 50%, ${color})`; 
 
   useEffect(() => {
       animate(color, COLORS, {
@@ -22,7 +22,7 @@ function Home() {
           duration: 10,
           repeat: Infinity,
           repeatType: 'mirror'
-      }); // Animate the color motion value through the COLORS array
+      }); 
   }, []);
 
 
@@ -30,9 +30,11 @@ function Home() {
     <motion.div
      className="home"
      style={{
-      backgroundImage,
-  }}
-     >
+      background: backgroundImage,
+      minHeight: '100vh', 
+      width: '100%', 
+    }}
+      >
       <Header/>
       <Academic/>
       <Professional/>
